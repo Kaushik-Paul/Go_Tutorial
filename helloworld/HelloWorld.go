bafb06fbe3cf075a4721e8ef6285b429004e5276
kaushik <kaushik.paul755@gmail.com>
2024-03-08 15:14:04 +0530

    Small assignment

--- helloworld/HelloWorld.go
+++ helloworld/HelloWorld.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World !!!")
+
+	// Even or odd numbers
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, num := range numbers {
+		if num%2 == 0 {
+			fmt.Printf("%v is even\n", num)
+		} else {
+			fmt.Printf("%v is odd\n", num)
+		}
+	}
 }
